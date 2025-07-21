c649b41fc72aa1d5639533c2425cae78b26b667f
Oleksandr Krolik <oskrolik@gmail.com>
2025-07-21 08:19:12 +0300

    Lesson number 2

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        title =  "Домашня сторінка",
+        heading = "Вітаю у моїй CMS",
+        message = "Це контент, переданий з Flask до HTML шаблону."
+    )
 
 if __name__ == ('__main__'):
     app.run(debug=True)
